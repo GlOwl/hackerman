a60d199189247665e059ed533c7753ac0170b542
Stefan Kremser <stef.1497@gmail.com>
2019-07-09 00:05:18 +0200

    Updated about page

--- website/content/about.php
+++ website/content/about.php
@@ -1,8 +1,10 @@
         <h3 class="my-4">About</h3>
         <p>
-          This is a fun side project, no money is being made here,
+          This is a pure fun project, no money is being made here,
           no personal data is being collected and no hackers were harmed producing this website.<br>
           <br>
           If you like to contribute, give feedback or fork the source code
           check us out on <a href="https://github.com/GlOwl/hackerman">GitHub</a>
+          <br>
+          Also make sure to follow <a href="http://twitter.com/hackermanwtf" target="_blank">@hackermanwtf</a> for your daily dose of hackerman quotes!
         </p>
