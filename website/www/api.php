ab989dce53d348ea484b6025565b63ee28aa3704
Stefan Kremser <stef.1497@gmail.com>
2019-07-08 20:44:06 +0200

    python -> python3

--- website/www/api.php
+++ website/www/api.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-type: application/json');
 $n = isset($_GET["n"]) ? $_GET["n"] : 1;
-echo shell_exec("python ../../generator/hackergenerator.py ".$n);
+echo shell_exec("python3 ../../generator/hackergenerator.py ".$n);
 ?>
