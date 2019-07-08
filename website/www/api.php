df1f08d81c837cb13b1e210464d25c26ab3085b5
Stefan Kremser <stef.1497@gmail.com>
2019-07-08 23:09:11 +0200

    Updated API to allow csv format

--- website/www/api.php
+++ website/www/api.php
@@ -1,5 +1,16 @@
 <?php
-header('Content-type: application/json');
 $n = isset($_GET["n"]) ? $_GET["n"] : 1;
-echo shell_exec("python3 ../../generator/hackergenerator.py ".$n." json");
+$f = isset($_GET["f"]) ? $_GET["f"] : "json";
+
+if($n < 0) $n = 1;
+if($n > 100) $n = 100;
+if($f != "json" && $f != "csv") $f = "json";
+
+if($f == "json") {
+  header('Content-type: application/json');
+} else if($f == "csv") {
+  header('Content-type: text/csv');
+}
+
+echo shell_exec("python3 ../../generator/hackergenerator.py ".$n." ".$f);
 ?>
