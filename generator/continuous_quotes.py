dceb53c92f95d95cf16a026d508e00f9f07e02e7
Stefan Kremser <stef.1497@gmail.com>
2019-07-09 09:41:51 +0200

    Arguments for interval and number

--- generator/continuous_quotes.py
+++ generator/continuous_quotes.py
@@ -1,7 +1,19 @@
 import hackergenerator
 import time
+import sys
+
+if(len(sys.argv) >= 2):
+    i = int(sys.argv[1])
+else:
+    i = 10
+    
+if(len(sys.argv) >= 3):
+    n = int(sys.argv[2])
+else:
+    n = 10
+
 while(True):
-    for x in range(0, 20):
+    for x in range(0, n):
         print(hackergenerator.sentence())
         print("")
-    time.sleep(10)
+    time.sleep(i)
