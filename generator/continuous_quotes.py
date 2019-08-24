5460d812895132b823f390cdf0447c2303be61f5
Anatol Pomplun <anatolpomplun@web.de>
2019-08-24 12:18:09 +0200

    More and updated Sentences.
    and one line for lazy people...

--- generator/continuous_quotes.py
+++ generator/continuous_quotes.py
@@ -1,12 +1,13 @@
 import hackergenerator
 import time
 import sys
+import os
 
 if(len(sys.argv) >= 2):
     i = int(sys.argv[1])
 else:
     i = 10
-    
+
 if(len(sys.argv) >= 3):
     n = int(sys.argv[2])
 else:
@@ -14,6 +15,8 @@ else:
 
 while(True):
     for x in range(0, n):
-        print(hackergenerator.sentence())
+        sentence = hackergenerator.sentence()
+        print(sentence)
+        #os.system("espeak '" + sentence + "'") #if you are to lazy to read install espeak and uncomment this line.
         print("")
     time.sleep(i)
